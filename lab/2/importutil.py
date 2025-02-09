fe24efff503a7eaf6fe052079aa6e440071c9cc8
lda <lda@ldlda.com>
2025-02-09 18:20:53 +0700

    they call me the quitter because i quit

---
+++ lab/2/importutil.py
@@ -0,0 +1,24 @@
+"ass"
+
+# do not tamper
+import importlib.util
+from pathlib import Path
+from sys import modules
+
+# oho
+
+
+def why_so_ass(file: Path | str, name: str, namesys: str = None):
+    """register file as module on sys.modules.
+    namesys is the one have dots in it its ok"""
+    # import importlib
+    if namesys is None:
+        namesys = name
+    if not file.is_file():
+        raise FileNotFoundError("where bro pointing to")
+    file = file.resolve(True)
+    spec = importlib.util.spec_from_file_location(name=name, location=file)
+
+    mod = importlib.util.module_from_spec(spec)
+    modules[namesys] = mod  # !!
+    spec.loader.exec_module(mod)
--- lab/2/newmult.py
+++ lab/2/newmult.py
@@ -1,1 +1,8 @@
 # no bro why tf does func return evaluation AND jacobian matrix WHAT EVEN IS JACOBIAN MATRIX
+# delegate this to code viewer, however to solve systems we can (and has already) do a def f(tuple) -> tuple
+
+
+# ok i have revised calc 2 (a thing i totally forgot) and HOW TF
+# DO YOU THINK I WOULD DO A multivariable DERIVATIVE???????
+# HOW?
+# in the slides they say symbolically SURE import sympy DO IT THEN
